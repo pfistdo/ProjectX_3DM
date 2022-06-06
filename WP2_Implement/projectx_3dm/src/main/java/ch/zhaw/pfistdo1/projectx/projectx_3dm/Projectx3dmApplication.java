b56cd364f7ce25bd5930c73dbe8ab4ae2f580d08
pfistdo1 <pfistdo1@students.zhaw.ch>
2022-06-06 19:53:44 +0200

    maven base + erd completed

---
+++ WP2_Implement/projectx_3dm/src/main/java/ch/zhaw/pfistdo1/projectx/projectx_3dm/Projectx3dmApplication.java
@@ -0,0 +1,13 @@
+package ch.zhaw.pfistdo1.projectx.projectx_3dm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projectx3dmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projectx3dmApplication.class, args);
+	}
+
+}
